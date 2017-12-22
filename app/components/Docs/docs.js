a129eac2e0af10ea19619432a2e941aea5581da9
liruidong <286182399@qq.com>
2017-12-22 18:53:03 +0800

    list

--- app/components/Docs/docs.js
+++ app/components/Docs/docs.js
@@ -1,6 +1,46 @@
 import React from "react";
+import '../menu.less';
+import '../main.less';
 import './docs.less';
-
+import Menu from '../Menu/menu';
+var arr = [
+	{
+		title : "title",
+		content : [
+			"menu1",
+			"menu2",
+			"menu3",
+			"menu4"
+		]
+	},
+	{
+		title : "title1",
+		content : [
+			"menu1",
+			"menu2",
+			"menu3",
+			"menu4"
+		]
+	},
+	{
+		title : "title2",
+		content : [
+			"menu1",
+			"menu2",
+			"menu3",
+			"menu4"
+		]
+	},
+	{
+		title : "title2",
+		content : [
+			"menu1",
+			"menu2",
+			"menu3",
+			"menu4"
+		]
+	}
+];
 export default class Hello extends React.Component{
 	constructor(props) {
         super(props);
@@ -9,7 +49,23 @@ export default class Hello extends React.Component{
 	render() {
 		return (
 			<div className="docs-components clearfix">
-				Hello World
+				<div className="main-components">
+					<div style={{width:"80%"}}>
+					<div className="title">Hello World</div>
+					<div className="main-docs">
+						<div className="introduce">
+						The easiest way to get started with React is to use this Hello World example code on CodePen. 
+						You don’t need to install anything; you can just open it in another tab and follow along as we go through examples. 
+						If you’d rather use a local development environment, check out the Installation page.
+						</div>
+					</div>
+					</div>
+				</div>
+				<div className="menu-components pos-f">
+					<div className="slideBar">
+						<Menu data={arr}/>
+					</div>
+				</div>
 			</div>
 		);
 	}
---
+++ app/components/Menu/menu.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './menu.less';
+
+
+var arr = [],
+	active = false;
+
+export default class accordion extends React.Component{
+	constructor(props) {
+	    super(props);
+	    //this.state = {isToggleOn: true};
+	    // 特别注意这一行语句
+	    //this.handleClick = this.handleClick.bind(this);
+	}
+	render() {
+		return (<div className="accordion" data={arr}>
+					{this.props.data.map((el, index)=> {
+						return (
+							<div className="div" key={index}>
+								<div className="title" onClick={this.handleClick.bind(null,index)}>{el.title}</div>
+								<ul className={"content "+(this.props.active ? "full" : "")} >
+									{el.content.map((option, index) => {
+											return (<li key={index}>{option}</li>)
+										}
+									)}
+								</ul>
+							</div>
+						)
+					})}
+		</div>)
+	}
+
+	handleClick(index , ev){
+		this.setState({
+			index : index
+		})
+	}
+
+}
